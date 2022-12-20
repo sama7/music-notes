01955a9f210f230cb3420730b1090f3e57c62ea0
sama7 <samahbinsaeed@gmail.com>
2022-12-20 01:12:44 -0500

    fix array index issue for playlist cover

--- client/src/components/PlaylistCoverImage.js
+++ client/src/components/PlaylistCoverImage.js
@@ -1,9 +1,18 @@
 export default function PlaylistCoverImage(props) {
+    function Image(props) {
+        if (props.currentPlaylistCover.images && props.currentPlaylistCover.images.length > 0) {
+            return (
+                <img
+                    className="playlist-cover"
+                    src={props.currentPlaylistCover.images[0].url}
+                    alt={`Cover for Playlist "${props.currentPlaylistCover.name}"`}
+                />
+            );
+        } else {
+            return;
+        }
+    }
     return (
-        <img
-            className="playlist-cover"
-            src={props.currentPlaylistCover.images[0].url}
-            alt={`Cover for Playlist "${props.currentPlaylistCover.name}"`}
-        />
+        <Image currentPlaylistCover={props.currentPlaylistCover} />
     );
 }
