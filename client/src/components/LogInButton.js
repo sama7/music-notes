f7435c193f63a5aed5f5a5e6410930dc57d9e049
Samah Saeed <samah.saeed@uconn.edu>
2023-03-22 22:43:02 -0400

    update base URLs for heroku

--- client/src/components/LogInButton.js
+++ client/src/components/LogInButton.js
@@ -1,10 +1,30 @@
 export default function LogInButton(props) {
+    const production = 'https://musicnotes.herokuapp.com';
+    const development = 'http://localhost:5000';
+
+    function Button(props) {
+        if (process.env.REACT_APP_ENV === 'production') {
+            return (
+                <a
+                    href={`${production}/login`}
+                    className="btn btn-primary login-btn"
+                >
+                    Log in
+                </a>
+            );
+        }
+        // otherwise, we're in dev
+        return (
+            <a
+                href={`${development}/login`}
+                className="btn btn-primary login-btn"
+            >
+                Log in
+            </a>
+        );
+    }
+
     return (
-        <a
-            href="http://localhost:5000/login"
-            className="btn btn-primary login-btn"
-        >
-            Log in
-        </a>
+        <Button />
     );
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config({ path: './config.env' });
 const cookieParser = require('cookie-parser');
@@ -8,11 +9,14 @@ const authorizeRouter = require('./routes/authorize');
 const noteRouter = require('./routes/note');
 const compression = require('compression');
 const port = process.env.PORT || 5000;
+const production = 'https://musicnotes.herokuapp.com';
+const development = 'http://localhost:3000';
+const base_url = (process.env.NODE_ENV ? production : development);
 
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: base_url,
     exposedHeaders: 'Retry-After',
 }));
 app.use(cookieParser());
@@ -24,9 +28,11 @@ app.use('/note', noteRouter);
 // get driver connection
 const dbo = require('./db/conn');
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(port, () => {
     // perform a database connection when server starts
